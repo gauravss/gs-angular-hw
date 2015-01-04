f4e7dd8c89e7c15b76fe135cb1cc7cc951432d11
Gaurav Sawant <gsawant@lendingclub.com>
2015-01-03 17:20:30 -0800

    step5 xhr & dependency injection

--- app/test/unit/controllersSpec.js
+++ app/test/unit/controllersSpec.js
@@ -1,28 +1,40 @@
 /**
  * Created by gsawant on 1/3/15.
  */
-describe('PhoneListController', function () {
+describe('myPhoneCat Controller', function() {
+    describe('PhoneListController', function () {
+        var scope, ctrl, $httpBackend;
 
-    var scope, ctrl;
+        beforeEach(module('myPhoneCatApp'));
 
-    beforeEach(module('myPhoneCatApp'));
+        beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/phones.json')
+                .respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
 
-    beforeEach(inject(function($controller){
-        scope = {};
-        ctrl = $controller('PhoneListController', {$scope:scope});
-    }));
+            scope = $rootScope.$new();
+            ctrl = $controller('PhoneListController', {$scope:scope});
+        }));
 
-    it('should create phones model with 3 phones', function() {
-        expect(scope.phones.length).toBe(3);
+        //it('should create phones model with 3 phones', function() {
+        //    expect(scope.phones.length).toBe(3);
+        //});
+
+
+        it('should create name as World', function() {
+            expect(scope.name).toBe('World');
+        });
+
+        it('should create "phones" model with 2 phones fetched from xhr', function() {
+           expect(scope.phones).toBeUndefined();
+            $httpBackend.flush();
+
+            expect(scope.phones).toEqual([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+        });
+
+        it('should set the defult value of orderProp model', function() {
+            expect(scope.orderProp).toBe('age');
+        });
+
     });
-
-
-    it('should create name as World', function() {
-        expect(scope.name).toBe('World');
-    });
-
-    it('should set the defult value of orderProp model', function() {
-        expect(scope.orderProp).toBe('age');
-    });
-
 });
